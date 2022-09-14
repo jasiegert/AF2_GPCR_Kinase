63e577ee6b8dc5dba0a464b3cb7a0c3ade0bbbe2
davidesala <d.sala1388@gmail.com>
2022-09-14 15:44:13 +0200

    debugging process_templates v18

--- scripts/mmseqs2.py
+++ scripts/mmseqs2.py
@@ -277,14 +277,11 @@ class MMSeqs2Runner:
                     r = requests.get( url )
                     rj = r.json()
                     
-                    if rj["state"]:
-        
-                        if rj["state"] == activation_state and len(pdbs) < 4:
-                            if pdbid not in check_duplicates:
-                                pdbs.append(pdb)
-                                check_duplicates.append(pdbid)
-                        if len(pdbs) == 4:
-                            break
+                    if type(rj) is dict and rj["state"] == activation_state and pdbid not in check_duplicates:
+                        pdbs.append(pdb)
+                        check_duplicates.append(pdbid)
+                    if len(pdbs) == 4:
+                        break
 			
                 elif pdb in templates:
                     pdbs.append(sl[1])
