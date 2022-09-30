23c7486eb572ba5812d334a889f88c51206ea460
davidesala <d.sala1388@gmail.com>
2022-09-30 22:11:19 +0200

    disperato

--- scripts/predict.py
+++ scripts/predict.py
@@ -348,13 +348,13 @@ def predict_structure_from_custom_template(
   print( f"\tMaximum number of recycling iterations: { max_recycles }" )
         
   pdb = protein.from_pdb_string(  util.pdb2str( template_pdb ) )
-
+  print(util.pdb2seq(template_pdb))
   tfeatures_in = {
     "template_aatype" : jax.nn.one_hot( pdb.aatype, 22 )[ : ][ None ],
     "template_all_atom_masks" : pdb.atom_mask[ : ][ None ],
     "template_all_atom_positions" : pdb.atom_positions[ :][ None ],
     "template_domain_names" : np.asarray( [ "None" ] ),
-    "template_sequence" : util.pdb2seq(template_pdb)}
+    "template_sequence" : }
 
 
   # Assemble the dictionary of input features
