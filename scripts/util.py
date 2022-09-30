382b2ce4932ede695ba8c70fb3d1917631f1997a
davidesala <d.sala1388@gmail.com>
2022-09-30 22:14:23 +0200

    un po meno disperato

--- scripts/util.py
+++ scripts/util.py
@@ -211,7 +211,7 @@ def remove_msa_for_custom_template_aligned_regions(feature_dict):
     mask = np.zeros(feature_dict['seq_length'][0], dtype=bool)
     for templ in feature_dict['template_sequence']:
         print(templ)
-        for i in len(list_seq):
+        for i in len(templ):
             print(i)
             mask[i] = True       
     feature_dict['deletion_matrix_int'][:,mask] = 0
