d342f63b51b13a0bb1f907898ea84686121c211d
davidesala <d.sala1388@gmail.com>
2022-09-30 21:33:29 +0200

    test final debug

--- scripts/util.py
+++ scripts/util.py
@@ -211,10 +211,10 @@ def remove_msa_for_template_aligned_regions(feature_dict):
 def remove_msa_for_custom_template_aligned_regions(feature_dict):
     mask = np.zeros(feature_dict['seq_length'][0], dtype=bool)
     for templ in feature_dict['template_sequence']:
-        for i,aa in enumerate(templ.decode("utf-8")):
-            if aa != '-':
-                mask[i] = True
-                
+        print(len(templ))
+        for i in len(templ):
+            print(i)
+            mask[i] = True            
     feature_dict['deletion_matrix_int'][:,mask] = 0
     feature_dict['msa'][:,mask] = 21
     return feature_dict
