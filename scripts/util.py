44c4776ee906bc7a57d5521bc97fb6ee8ea0d805
davidesala <d.sala1388@gmail.com>
2022-09-30 21:14:20 +0200

    test final debug

--- scripts/util.py
+++ scripts/util.py
@@ -212,6 +212,7 @@ def remove_msa_for_custom_template_aligned_regions(feature_dict):
     mask = np.zeros(feature_dict['seq_length'][0], dtype=bool)
     for templ in feature_dict['template_sequence']:
         for i,aa in enumerate(templ):
+            print(i, aa)
             if aa != '-':
                 mask[i] = True
                 
