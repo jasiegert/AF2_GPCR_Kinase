ebf79d9ef9ae8566ffea4abde65e54447f532bee
davidesala <d.sala1388@gmail.com>
2022-09-30 20:11:29 +0200

    te

--- scripts/predict.py
+++ scripts/predict.py
@@ -355,7 +355,7 @@ def predict_structure_from_custom_template(
     "template_all_atom_positions" : pdb.atom_positions[ :][ None ],
     "template_domain_names" : np.asarray( [ "None" ] ) }
 
-  print(tfeatures_in["template_aatype"])
+  print('TEFEAT:' tfeatures_in["template_aatype"])
   # Assemble the dictionary of input features
   features_in = util.setup_features(
       seq, a3m_lines, tfeatures_in)
