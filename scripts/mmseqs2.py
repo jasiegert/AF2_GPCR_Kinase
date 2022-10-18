1aeefd3c95efdb2dc56272f3daadc493b2d56336
davidesala <d.sala1388@gmail.com>
2022-10-18 10:41:57 +0200

    debugging pdb selection

--- scripts/mmseqs2.py
+++ scripts/mmseqs2.py
@@ -262,11 +262,13 @@ class MMSeqs2Runner:
         with open(f"{ self.path }/pdb70.m8", "r") as infile:
 
             for line in infile:
-
+                 
                 sl = line.rstrip().split()
                 pdb = sl[1]
                 pdbid = pdb.split("_")[0]
-                
+                print('DEBUG SL', sl)
+                print('DEBUG PDB', pdb)
+                print('DEBUG PDBID', pdbid)
                 if templates:
                     if templates[0] in ["Active", "Inactive", "Intermediate", "G protein", "Arrestin"] and pdbid not in check_duplicates and pdbid not in templates:
                         
